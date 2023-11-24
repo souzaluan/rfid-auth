608afdce86c2885cf7cb28750f126d450401282e
souzaluan <luan.souza69@hotmail.com>
2023-11-23 21:11:12 -0300

    feat: application repository container

--- api/src/container/index.ts
+++ api/src/container/index.ts
@@ -4,9 +4,15 @@ import { IUsersRepository } from '../modules/users/repositories/IUsersRepository
 import { UsersRepository } from '../modules/users/infra/typeorm/repositories/UsersRepository'
 import { ITagsRepository } from '../modules/tags/repositories/ITagsRepository'
 import { TagsRepository } from '../modules/tags/infra/typeorm/repositories/TagsRepository'
+import { IApplicationsRepository } from '../modules/applications/repositories/IApplicationsRepository'
+import { ApplicationsRepository } from '../modules/applications/infra/typeorm/repositories/ApplicationsRepository'
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
 )
 container.registerSingleton<ITagsRepository>('TagsRepository', TagsRepository)
+container.registerSingleton<IApplicationsRepository>(
+  'ApplicationsRepository',
+  ApplicationsRepository,
+)
