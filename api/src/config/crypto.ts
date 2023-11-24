368c68ff36bc227e20ea5692549848b2aafb6d5f
souzaluan <luan.souza69@hotmail.com>
2023-11-23 21:59:35 -0300

    refactor: remove unused crypto initialization

--- api/src/config/crypto.ts
+++ api/src/config/crypto.ts
@@ -2,5 +2,4 @@ export const cryptoConfig = {
   algorithm: process.env.CRYPTO_ALGORITHM as string,
   type: process.env.CRYPTO_TYPE as BufferEncoding,
   secret: process.env.CRYPTO_SECRET as string,
-  initialization: process.env.CRYPTO_INITIALIZATION as string,
 }
